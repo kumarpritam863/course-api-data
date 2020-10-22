f33a6e5c5a6b88226e9e067421610d231859cb7c
pritam <kumarpritam863@gmail.com>
2020-10-22 14:52:57 +0530

    Simple Spring Project with Database Integration.

---
+++ src/main/java/com/example/pritam/kumar2/courseapidata/CourseApiDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.pritam.kumar2.courseapidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/topic/TopicService.java
@@ -0,0 +1,40 @@
+package topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("Spring","Spring Framework","Spring FrameWork Description"),
+            new Topic("java","SCore Java","Core Java Description"),
+            new Topic("JavaScript","JavaScript Framework","JavaScript Description")
+    ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+    public Topic getTopic(String id){
+        return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i = 0; i<topics.size(); i++){
+            if(topics.get(i).getId().equals(id)){
+                topics.set(i,topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t->t.getId().equals(id));
+    }
+}
